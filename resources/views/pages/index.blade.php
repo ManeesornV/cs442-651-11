04849ccfbd1bfc425853bba14b345d22b278e9b5
ManeesornV <maneesorn.v@ku.th>
2022-07-19 08:21:31 +0700

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>List of All pages</h1>
+
+    @for($i = 1; $i <= 10; $i++)
+        <div>
+            <a href="{{ url("/pages/{$i}") }}">
+                page{{$i}} 
+            </a>
+        </div>
+    @endfor
+
+@endsection
